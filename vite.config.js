268dbeea438029ab2b8a5a5a956e77920af095f7
Andrew Mueller <amueller@caltech.edu>
2024-02-08 17:18:51 -0800

    updating paths

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  bas: "/spdc_cavity_analysis/",
+  base: "/spdc_cavity_analysis/",
   plugins: [svelte()],
 })
